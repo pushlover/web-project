6ff3df02a1622b32cbf10a08ceb79a4eda259436
pushlover <38527861+pushlover@users.noreply.github.com>
2021-04-19 19:48:14 +0800

    Update leetcode112.js

--- leetcode112.js
+++ leetcode112.js
@@ -1,4 +1,11 @@
 // https://leetcode-cn.com/problems/path-sum/
+// 给你二叉树的根节点 root 和一个表示目标和的整数 targetSum ，判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum 。
+
+// 叶子节点 是指没有子节点的节点。
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/path-sum
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
